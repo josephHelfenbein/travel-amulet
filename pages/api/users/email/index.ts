a07a797568b1aa1cb6b6d9ee86209a68af221c46
Joseph J Helfenbein <josephhelfenbein@gmail.com>
2024-07-31 00:00:23 -0400

    fix hopefully

--- pages/api/users/email/index.ts
+++ pages/api/users/email/index.ts
@@ -6,7 +6,7 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
-    const {email} = req.query;
-    const user = await prisma.user.findUnique({where: {email: email}});
+    const email = req.query.toString();
+    const user = await prisma.user.findUnique({where: {email:email}});
     return user ? res.send(user) : res.status(400).end();
 }
