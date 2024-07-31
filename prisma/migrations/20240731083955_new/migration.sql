8e8ec72b638ed46357ef4fd250e30b1b8551f0c4
Joseph J Helfenbein <josephhelfenbein@gmail.com>
2024-07-31 04:40:14 -0400

    possible fix

--- prisma/migrations/20240731083955_new/migration.sql
+++ prisma/migrations/20240731083955_new/migration.sql
@@ -5,7 +5,9 @@
   - A unique constraint covering the columns `[email]` on the table `users` will be added. If there are existing duplicate values, this will fail.
   - Made the column `stock` on table `books` required. This step will fail if there are existing NULL values in that column.
   - Made the column `price` on table `books` required. This step will fail if there are existing NULL values in that column.
+  - Added the required column `country` to the `users` table without a default value. This is not possible if the table is not empty.
   - Added the required column `email` to the `users` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `name` to the `users` table without a default value. This is not possible if the table is not empty.
   - Added the required column `password` to the `users` table without a default value. This is not possible if the table is not empty.
   - Made the column `balance` on table `users` required. This step will fail if there are existing NULL values in that column.
 
@@ -15,7 +17,9 @@ ALTER TABLE `books` MODIFY `stock` INTEGER NOT NULL DEFAULT 0,
     MODIFY `price` DECIMAL(15, 2) NOT NULL DEFAULT 0.0;
 
 -- AlterTable
-ALTER TABLE `users` ADD COLUMN `email` VARCHAR(255) NOT NULL,
+ALTER TABLE `users` ADD COLUMN `country` VARCHAR(2) NOT NULL,
+    ADD COLUMN `email` VARCHAR(255) NOT NULL,
+    ADD COLUMN `name` VARCHAR(255) NOT NULL,
     ADD COLUMN `password` VARCHAR(255) NOT NULL,
     MODIFY `balance` DECIMAL(15, 2) NOT NULL DEFAULT 0.0;
 
