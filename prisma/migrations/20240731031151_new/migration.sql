d489890ee3133061d73110426ec1964200608a58
Joseph J Helfenbein <josephhelfenbein@gmail.com>
2024-07-30 23:12:20 -0400

    test2

---
+++ prisma/migrations/20240731031151_new/migration.sql
@@ -0,0 +1,26 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[id]` on the table `users` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[email]` on the table `users` will be added. If there are existing duplicate values, this will fail.
+  - Made the column `stock` on table `books` required. This step will fail if there are existing NULL values in that column.
+  - Made the column `price` on table `books` required. This step will fail if there are existing NULL values in that column.
+  - Added the required column `email` to the `users` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `password` to the `users` table without a default value. This is not possible if the table is not empty.
+  - Made the column `balance` on table `users` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- AlterTable
+ALTER TABLE `books` MODIFY `stock` INTEGER NOT NULL DEFAULT 0,
+    MODIFY `price` DECIMAL(15, 2) NOT NULL DEFAULT 0.0;
+
+-- AlterTable
+ALTER TABLE `users` ADD COLUMN `email` VARCHAR(255) NOT NULL,
+    ADD COLUMN `password` VARCHAR(255) NOT NULL,
+    MODIFY `balance` DECIMAL(15, 2) NOT NULL DEFAULT 0.0;
+
+-- CreateIndex
+CREATE UNIQUE INDEX `users_id_key` ON `users`(`id`);
+
+-- CreateIndex
+CREATE UNIQUE INDEX `users_email_key` ON `users`(`email`);
