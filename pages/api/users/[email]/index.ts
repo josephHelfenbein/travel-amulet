02ecc0c186d4dad799e5913f123365d14b935e82
Joseph J Helfenbein <josephhelfenbein@gmail.com>
2024-08-01 05:43:27 -0400

    should fix

--- pages/api/users/[email]/index.ts
+++ pages/api/users/[email]/index.ts
@@ -6,8 +6,7 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
-    const {id} = req.query;
-    const email = id.toString();
+    const email = req.query.toString();
     const user = await prisma.user.findUnique({where: {email:email}});
     return user ? res.send(user) : res.status(400).end();
 }
