6c51e4a4346f879f45d71153df62a2b517d260f8
Joseph J Helfenbein <josephhelfenbein@gmail.com>
2024-08-01 21:50:02 -0400

    test fix

--- pages/api/users/[email]/index.ts
+++ pages/api/users/[email]/index.ts
@@ -6,7 +6,9 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
-    const email = req.query.toString();
+    const {newQuery} = req.query;
+    const email = newQuery?.toString();
     const user = await prisma.user.findFirst({where: {email: email,}});
+    
     return user ? res.send(user) : res.status(400).end();
 }
