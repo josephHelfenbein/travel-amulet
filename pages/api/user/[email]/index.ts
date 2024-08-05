0999958d7cf18bdc8b28f5efac9409197c7e893a
Joseph J Helfenbein <josephhelfenbein@gmail.com>
2024-08-05 03:00:02 -0400

    debug 13

--- pages/api/user/[email]/index.ts
+++ pages/api/user/[email]/index.ts
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 import prisma from '../../../../lib/prisma';
-import exclude from '../login';
+import exclude from '../auth';
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
