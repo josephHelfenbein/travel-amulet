66cd3a6bc52586af0ca2d2de6e34f472c155aa73
Joseph J Helfenbein <josephhelfenbein@gmail.com>
2024-08-06 00:02:06 -0400

    useState test

--- src/components/accountsettings.tsx
+++ src/components/accountsettings.tsx
@@ -1,6 +1,7 @@
 import {useRouter} from "next/router";
 import {useSession} from 'next-auth/react';
 import {fetchUserByEmail} from '../../lib/http';
+import { useState } from "react";
 
 export default function AccountSettings(){
     const router = useRouter();
@@ -11,12 +12,14 @@ export default function AccountSettings(){
         },
     });
     const sessionEmail = data?.user?.email!;
+    const [name, setName] = useState('');
     const sessionUser = async () => {
-        await fetchUserByEmail(sessionEmail?.toString());
+        const nameAwait = await fetchUserByEmail(sessionEmail?.toString());
+        setName(nameAwait?.content?.name!);
     }
     return (
         <div className='card p-5'>
-            <h1 className="display-6 mb-3">Hello, {sessionUser.name}</h1>
+            <h1 className="display-6 mb-3">Hello, {name}</h1>
         </div>
     );
 }
