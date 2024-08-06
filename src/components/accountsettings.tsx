ad2442233b226a1a1ee9dbc318e235a1603ac65a
Joseph J Helfenbein <josephhelfenbein@gmail.com>
2024-08-06 02:09:23 -0400

    debug 14

--- src/components/accountsettings.tsx
+++ src/components/accountsettings.tsx
@@ -4,20 +4,16 @@ import { useState } from "react";
 import { getSession } from "next-auth/react";
 export default function AccountSettings(){
     const router = useRouter();
-    const {data:session, data:token, status} = useSession({
+    const {data:session, status} = useSession({
         required:true,
         onUnauthenticated() {
             router.push('/login');
         },
     });
     const [name, setName] = useState('');
-    async function sessionUser () {
-        console.log(token);
-        console.log(token?.user);
-        console.log(token?.user?.name);
-    }
-    sessionUser();
-    
+    console.log('1. ' + session);
+    console.log('2. ' + session?.user);
+    console.log('3. ' + session?.user?.name);
     return (
         <div className='card p-5'>
             <h1 className="display-6 mb-3">Hello, {name}</h1>
