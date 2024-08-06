9f63641d5f0068deed632f2e874f5a18ae62329e
Joseph J Helfenbein <josephhelfenbein@gmail.com>
2024-08-06 03:54:19 -0400

    debug 32

--- src/components/accountsettings.tsx
+++ src/components/accountsettings.tsx
@@ -2,6 +2,7 @@
 import {useRouter} from "next/router";
 import {useSession} from 'next-auth/react';
 import { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function AccountSettings(){
     const router = useRouter();
@@ -11,21 +12,18 @@ export default function AccountSettings(){
             router.push('/login');
         },
     });
-    const [session, setSession] = useState(data);
+    const [session, setSession] = useState('');
     useEffect(() => {
-        const fetchSession = async () => {
-            const response = await fetch('/api/auth/session');
-            const session = await response.json();
-            setSession(session);
-        };
-        fetchSession();
+        axios.get('/api/auth/session').then((res) =>{
+            setSession(res.data.session.user.name);
+            console.log(res.data.session);
+            console.log(res.data.session.user);
+        })
     }, []);
-    console.log(session);
-    console.log(session?.user);
 
     return (
         <div className='card p-5'>
-            <h1 className="display-6 mb-3">Hello, {session?.user?.name}</h1>
+            <h1 className="display-6 mb-3">Hello, {session}</h1>
         </div>
     );
 }
