def0fee3772c56e0a4b0ad43bc32f1e31af3f5ec
Joseph J Helfenbein <josephhelfenbein@gmail.com>
2024-08-13 04:08:14 -0400

    debug 5

--- src/components/signup-form.tsx
+++ src/components/signup-form.tsx
@@ -123,7 +123,8 @@ const SignUpForm = () => {
                     </div>
                 }
                 <button type="button" onClick={async ()=>{
-                    let res = await signIn("google", {callbackUrl: `/account`,})
+                    "use server";
+                    await signIn("google", {callbackUrl: `/account`,});
                 }} style={{width:'265px'}} id={styles.google} className={styles.buttonLogin+" btn btn-primary mt-3"}>
                     <svg xmlns="http://www.w3.org/2000/svg" style={{marginRight:"5px"}} height="24" viewBox="0 0 24 24" >
                         <path d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" fill="#4285F4"></path>
