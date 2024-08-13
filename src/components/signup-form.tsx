e948a5beeb220bc8071822820666103799907286
Joseph J Helfenbein <josephhelfenbein@gmail.com>
2024-08-13 05:01:01 -0400

    added github provider test

--- src/components/signup-form.tsx
+++ src/components/signup-form.tsx
@@ -131,7 +131,7 @@ const SignUpForm = () => {
                         <path d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z" fill="#FBBC05"></path>
                         <path d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z" fill="#EA4335"></path>
                     </svg>
-                    Or, Sign in with Google</button>
+                    Sign in with Google</button>
             </Form>
         </Formik>
     </div>);
--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -2,6 +2,7 @@ import NextAuth, {AuthOptions, getServerSession} from 'next-auth';
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
 import CredentialsProvider from 'next-auth/providers/credentials';
 import GoogleProvider from 'next-auth/providers/google';
+import GithubProvider from 'next-auth/providers/github';
 import prisma from '../../../../lib/prisma';
 
 let userAcc;
@@ -42,6 +43,11 @@ const options = {
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
+    GithubProvider({
+      id: "github",
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
   ],
 
   adapter: PrismaAdapter(prisma),
@@ -73,7 +79,7 @@ const options = {
         return await token;
     },
     async signIn({user, account}){
-      if(account?.provider==="google"){
+      if(account.provider==="google" || account.provider==="github"){
         const {email, name, image, id} = user;
         let userExists = await fetchUserExistsEmail(email);
         if(!userExists) {
@@ -96,7 +102,7 @@ const options = {
         }
         return true;
       }
-      if(account?.provider === "credentials") return true;
+      if(account.provider === "credentials") return true;
       else return false;
     }
   },
