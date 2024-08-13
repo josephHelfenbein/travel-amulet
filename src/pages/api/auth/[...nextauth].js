75b6b1a4c71d2bfe461400fa0ed4fd1ba3ba5e10
Joseph J Helfenbein <josephhelfenbein@gmail.com>
2024-08-13 17:49:39 -0400

    debug 3

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -43,12 +43,14 @@ const options = {
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET, 
       allowDangerousEmailAccountLinking: true,
+      checks: ['none'],
     }),
     GithubProvider({
       id: "github",
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
       allowDangerousEmailAccountLinking: true,
+      checks: ['none'],
     }),
   ],
 
