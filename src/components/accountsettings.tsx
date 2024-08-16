9626423e105bdabb025a52364e30fb0123acf0bd
Joseph J Helfenbein <josephhelfenbein@gmail.com>
2024-08-16 17:59:01 -0400

    results showing on accounts page

--- src/components/accountsettings.tsx
+++ src/components/accountsettings.tsx
@@ -10,6 +10,26 @@ import { countryOptions } from "./countryoptions";
 import validationSchema from "./validationSchema";
 import styles from './login-form.module.css';
 
+const countriesMap = new Map();
+for(let i=0; i<countryOptions.length; i++){
+    const newName = countryOptions[i].label.substring(0, countryOptions[i].label.length);
+    countriesMap.set(countryOptions[i].value, newName);
+}
+function countryCodeToName(code:string){
+    return countriesMap.get(code);
+}
+function resultsList(results:string){
+    let returnList = [<div></div>];
+    for(let i=0; i<results.length; i+=2){
+        returnList.push(
+            <div style={{backgroundColor:'#fefefe', borderRadius: '10px', borderStyle: 'solid', border: '2px solid #e4e4e4'}} className="mb-1 m-3 p-3">
+                <p className='mb-1'>{countryCodeToName(results.substring(i, i+2))}</p>
+            </div>
+        )
+    }
+    return returnList;
+}
+
 export default function AccountSettings(){
     const router = useRouter();
     const {data, status, update} = useSession({
@@ -22,6 +42,9 @@ export default function AccountSettings(){
     const [email, setEmail] = useState('');
     const [country, setCountry] = useState('');
     const [error, setError] = useState('');
+
+    const [showResults, setResultsShow] = useState(false);
+    const [results, setResults] = useState('');
     useEffect(() => {
         axios.get('/api/auth/session').then(async (res) =>{
             if(res){
@@ -30,6 +53,8 @@ export default function AccountSettings(){
                 const userRes = await axios.get(`/api/user/${res.data.session.user.email}`)
                 const countryStr = userRes.data.country;
                 setCountry(countryStr);
+                const resultsStr = userRes.data.results;
+                setResults(resultsStr);
             }
         })
     }, []);
@@ -105,6 +130,25 @@ export default function AccountSettings(){
                     {error != '' &&
                     <p className="mt-2">{error}</p>
                     }
+                     <div style={{backgroundColor:'#fefefe', borderRadius: '10px', borderStyle: 'solid', border: '2px solid #e4e4e4'}} className="mb-1 m-3 p-3">
+                    <a onClick={()=>{
+                        setResultsShow(!showResults);
+                    }} className='d-flex justify-content-center p-1'>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="#e08550" height="52px" >
+                            <path d="M440-760v-160h80v160h-80Zm266 110-56-56 113-114 56 57-113 113Zm54 210v-80h160v80H760Zm3 299L650-254l56-56 114 112-57 57ZM254-650 141-763l57-57 112 114-56 56Zm-14 450h180q25 0 42.5-17.5T480-260q0-25-17-42.5T421-320h-51l-20-48q-14-33-44-52.5T240-440q-50 0-85 35t-35 85q0 50 35 85t85 35Zm0 80q-83 0-141.5-58.5T40-320q0-83 58.5-141.5T240-520q60 0 109.5 32.5T423-400q58 0 97.5 43T560-254q-2 57-42.5 95.5T420-120H240Zm320-134q-5-20-10-39t-10-39q45-19 72.5-59t27.5-89q0-66-47-113t-113-47q-60 0-105 39t-53 99q-20-5-41-9t-41-9q14-88 82.5-144T480-720q100 0 170 70t70 170q0 77-44 138.5T560-254Zm-79-226Z"></path>
+                    </svg>
+                    <h5  className="bd-highlight p-2" style={{textAlign:'center', fontWeight:400, color:'#505050'}}>Previous Results</h5>
+                    {!showResults && <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="#a0a0a0" height="32px" >
+                            <path d="M480-344 240-584l56-56 184 184 184-184 56 56-240 240Z"></path>
+                    </svg>}
+                    {showResults && <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="#a0a0a0" height="32px" >
+                            <path d="M480-528 296-344l-56-56 240-240 240 240-56 56-184-184Z"></path>
+                    </svg>}
+                    </a>
+                    {showResults && 
+                        resultsList(results)
+                        }
+                    </div>
                 </Form>
                 
             </Formik>
