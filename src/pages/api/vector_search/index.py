5825f7419ce0f8708d92941231ac0e09a884f1a3
YashedP <yashjani144@gmail.com>
2024-08-17 04:11:45 -0400

    i shall call it debug now, debug 4

--- src/pages/api/vector_search/index.py
+++ src/pages/api/vector_search/index.py
@@ -3,9 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/api/vector_search')
-def home():
-    return 'Hello, World!'
+def vector_search():
+    return'Hello, World!'
 
 # @app.route('/about')
 # def about():
-#     return 'About'
+#   return 'About'
