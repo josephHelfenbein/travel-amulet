61d89135f062d6c06d9ee219c7d6517af2ab15ee
YashedP <yashjani144@gmail.com>
2024-08-16 22:14:43 -0400

    Upstage... uhh... vector search shenanigans?

--- Vector Search/CountryData.py
+++ Vector Search/CountryData.py
@@ -14,48 +14,70 @@ payload = "countries=us"
 
 response = requests.request("GET", reqUrl, data=payload,  headers=headersList)
 
-output_file = open("country_data.txt", "w", encoding="utf-8")
+output_file = open("country_data_markdown.txt", "w", encoding="utf-8")
+output_file2 = open("country_metadata.txt", "w", encoding="utf-8")
 
 for country in response.json():
+    countryName = country.get('name')
+    continent = join_with_spaces(country.get('continent'));
+    officialLanguages = join_with_spaces(country.get('languages').get('official'))
+    widelySpokenLanguages = country.get('languages').get('widely_spoken')
+    countryIntro = country.get('textual').get('intro')[0]
+    countryClimate = country.get('textual').get('climate')[0]
+    countryCuisine = country.get('textual').get('cuisine')[0]
+    countryReligions = join_with_spaces(country.get('religions').get('labels'))
+    countryCrimeIndex = None
+    try:
+        countryCrimeIndex = country.get('crime_index').get('datasets').get('data')[0]
+    except:
+        countryCrimeIndex = ""
+    countryInternetSpeed = country.get('internet_speed').get('broadband').get('download').get('value')
+    
+    countryTapWaterIndex = None
+    try:
+        countryTapWaterIndex = country.get('tap_water').get('datasets').get('data')[0]
+    except:
+        countryTapWaterIndex = "-1"
+
+    countryPolitics = country.get('textual').get('politics')[0]
     string = ""
 
     # Continent
-    countryName = country.get('name')
-    string += f"{countryName} is located in {join_with_spaces(country.get('continent'))}. " 
+    string += f"{countryName} is located in {continent}. " 
 
     # Language
-    string += f"Speaks {join_with_spaces(country.get('languages').get('official'))} officially"
-    if (len(country.get('languages').get('widely_spoken')) > 0):
-        string += f" and speaks {join_with_spaces(country.get('languages').get('widely_spoken'))} widely"
+    string += f"Speaks {officialLanguages} officially"
+    if len(widelySpokenLanguages) > 0:
+        string += f" and speaks {join_with_spaces(widelySpokenLanguages)} widely"
     string += ". "
 
     # Has Landmarks
-    string += f"{country.get('textual').get('intro')[0]} "
+    string += f"{countryIntro} "
 
     # Climate/Temperature
-    string += f"{country.get('textual').get('climate')[0]} "
+    string += f"{countryClimate} "
     
     # Food
-    string += f"{country.get('textual').get('cuisine')[0]} "
+    string += f"{countryCuisine} "
 
     # Religion
-    string += f"{countryName} believes in {join_with_spaces(country.get('religions').get('labels'))}. "
+    string += f"{countryName} believes in {countryReligions}. "
 
     # Crime Index
-    if len(country.get('crime_index').get('datasets').get('data')) == 0:
+    if countryCrimeIndex == "":
         string += f"Does not have a crime index available but is dangerous. "
     else:
-        string += f"Has as crime index of {country.get('crime_index').get('datasets').get('data')[0]}. "
+        string += f"Has as crime index of {countryCrimeIndex}. "
         
     # Download Speed
-    if country.get('internet_speed').get('broadband').get('download').get('value') == "":
+    if countryInternetSpeed == "":
         string += f"Does not have a broadband download speed available. "
     else:
-        string += f"Has a broadband download speed of {country.get('internet_speed').get('broadband').get('download').get('value')} Mbps. "
+        string += f"Has a broadband download speed of {countryInternetSpeed} Mbps. "
 
     # Tap Water Index
-    if len(country.get('tap_water').get('datasets').get('data')) > 0:
-        string += f"Has a tap water index of {country.get('tap_water').get('datasets').get('data')[0]}. "
+    if int(countryTapWaterIndex) > 0:
+        string += f"Has a tap water index of {countryTapWaterIndex}. "
     elif countryName == "Nauru":
         string += f"Does not have a tap water index available but is safe to drink. "
     elif countryName == "Palestine State":
@@ -78,9 +100,16 @@ for country in response.json():
         string += f"Does not have a tap water index available but is not safe to drink. "
 
     # Govt has Voting System and conflicts
-    string += f"{country.get('textual').get('politics')[0]} "
-    output_file.write(string + "\n");
-    # try:
-    #     output_file.write(string + "\n");
-    # except:
-    #     print(string)
+    string += f"{countryPolitics}"
+
+    output_file.write(f"# {countryName}\n")
+    output_file.write(string + "\n\n")
+    
+    output_file2.write(f"{countryName}\n")
+    output_file2.write(f"Continent: {continent}\n")
+    output_file2.write(f"Crime Index: {countryCrimeIndex}\n")
+    if countryInternetSpeed != "":
+        output_file2.write(f"Download Speed: {countryInternetSpeed}\n")
+    if countryTapWaterIndex != "-1":
+        output_file2.write(f"Tap Water Index: {countryTapWaterIndex}\n")
+    output_file2.write("\n")
